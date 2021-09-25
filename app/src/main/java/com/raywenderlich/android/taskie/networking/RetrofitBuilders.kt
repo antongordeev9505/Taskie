e64f37799857c1085f42137ce130e68613cb52ad
antongordeev <antongordeev9505@gmail.com>
2021-09-25 17:42:55 +0300

    Use Authorization Interceptor to have by default authorization with every Api call. Remove Headers with authorization from Api calls from RemoteApiService.kt

--- app/src/main/java/com/raywenderlich/android/taskie/networking/RetrofitBuilders.kt
+++ app/src/main/java/com/raywenderlich/android/taskie/networking/RetrofitBuilders.kt
@@ -1,13 +1,17 @@
 package com.raywenderlich.android.taskie.networking
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.raywenderlich.android.taskie.App
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 
+private const val HEADER_AUTHORIZATION = "Authorization"
 //default client
 fun buildClient(): OkHttpClient =
     OkHttpClient.Builder()
@@ -15,7 +19,28 @@ fun buildClient(): OkHttpClient =
         .addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         })
+        .addInterceptor(buildAuthorizationInterceptor())
         .build()
+
+fun buildAuthorizationInterceptor() = object : Interceptor {
+    //interceptor receive interceptor chain
+    //chain - all the layers APICall goes through before returning response
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        //get original request
+        val originalRequest = chain.request()
+        //check existence of token, if there is not - proceed with original request
+        if (App.getToken().isBlank()) return chain.proceed(originalRequest)
+
+        //if there is token - create new request with header
+        val new = originalRequest.newBuilder()
+            .addHeader(HEADER_AUTHORIZATION, App.getToken())
+            .build()
+        //this will be done with all requests - means we will be authorized for all calls
+
+        return chain.proceed(new)
+    }
+}
 
 //make more forgiving parser
 private var json = Json {
